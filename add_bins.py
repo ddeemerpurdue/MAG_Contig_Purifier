bd9a48e70f62e8bd046a6b9709ab6842d33713f2
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-07-22 14:46:55 -0400

    Update add_bins.py

--- add_bins.py
+++ add_bins.py
@@ -1,6 +1,31 @@
 '''
--BLAH
+Program designed to take in the raw input from the fastANI snakemake
+pipeline and standardize it with bin information from RefineM output.
+Input requires a tab-delimited list of the binfiles as well.
+Example useage:
+$ python add_bins.py -a ani.txt -o out.txt -b bin1.txt bin2.txt
+
+Note: bin files need to be in the specified format: sample.Bins.txt
 '''
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Parser")
+parser.add_argument("-b", "--Bins", required=True, nargs="*",
+                    help="One or more files with contig name and bin info")
+
+parser.add_argument("-a", "--ANI", help="ANI file created from FASTANI",
+                    required=True)
+parser.add_argument("-o", "--Output", help="Standardized ANI file to write to",
+                    required=True)
+argument = parser.parse_args()
+
+
+binlist = []
+for b in argument.Bins:
+    binlist.append(b)
+print(binlist)
+
 
 def get_bindict(binfile):
     # Start with query bins
@@ -8,77 +33,60 @@ def get_bindict(binfile):
     with open(binfile) as qf:
         line = qf.readline()
         while line:
-            node = line.split('\t')[0].split('_')[1]
-            node = str(int(node))
-            bin_name = line.split('\t')[1].strip()
+            bin_name = line.split('\t')[0].split('.')[1].split('_')[0]
+            node = line.split('\t')[1].strip()
+            node = node.strip('>')
             bindict[node] = bin_name
             line = qf.readline()
     return bindict
 
 
-def get_all_bindicts(binlist):
-    master = {}
-    for bins in binlist:
-        name = bins.split('/')[1].split('-')[0]
-        print(name)
-        master[name] = get_bindict(bins)
-    return master
+def get_all_bindict(binlist):
+    all_dict = {}
+    for file in binlist:
+        dic = get_bindict(file)
+        key = str(file).split('.')[0]
+        all_dict[key] = dic
+    return all_dict
 
 
-def append_bins_to_ani(binlist, anifile):
+def append_bins_to_ani(binfile, anifile, output):
     writelist = []
-    bins = get_all_bindicts(binlist)
+    bins = get_all_bindict(binfile)
     # Parse through ANI file
-    with open(anifile) as af:
-        constant_line = af.readline()
-        while constant_line:
-            line = constant_line.split('\t')
-            quer_num = line[0].split('/')[2].split('_')[1]
-            ref_num = line[1].split('/')[2].split('_')[1]
-            query = line[0].split('/')[1]
-            reference = line[1].split('/')[1]
-            quer_node = line[0].split('/')[2].rsplit('.', 1)[0]
-            reference_node = line[1].split('/')[2].rsplit('.', 1)[0]
-            ani = line[2]
-            orths = line[3]
-            total = line[4].strip()
-            # print(f"Query={query}, Ref={reference}, Nums={quer_num}\t{ref_num},\nNodes:\t{quer_node}\t{reference_node}\nANI:\t{ani}")
-            # See if there's a bin for the nodes
-            if quer_num in bins[query]:
-                quer_bin = bins[query][quer_num]
-            else:
-                quer_bin = 'NoBin'
-            # Repeat for reference
-            if ref_num in bins[reference]:
-                ref_bin = bins[reference][ref_num]
-            else:
-                ref_bin = 'NoBin'
-            # Write all information to a file
-            new_info = '\t'.join([query, reference, quer_node, reference_node,
-                                  ani, orths, total, quer_bin, ref_bin])
-            writelist.append(new_info)
+    with open(output, 'w') as o:
+        with open(anifile) as af:
             constant_line = af.readline()
+            while constant_line:
+                line = constant_line.split('\t')
+                quer = line[0].split('/')[1].split('_')[0]
+                refer = line[1].split('/')[1].split('_')[0]
+                if quer != refer:
+                    quer_node = line[0].split('/')[2].rsplit('.', 1)[0]
+                    reference_node = line[1].split('/')[2].rsplit('.', 1)[0]
+                    print(f"Qnode: {quer_node}\tRNode: {reference_node}")
+                    ani = line[2]
+                    orths = line[3]
+                    total = line[4].strip()
+                    if quer_node in bins[quer]:
+                        quer_bin = bins[quer][quer_node]
+                    else:
+                        quer_bin = 'NoBin'
+                    # Repeat for reference
+                    if reference_node in bins[refer]:
+                        ref_bin = bins[refer][reference_node]
+                    else:
+                        ref_bin = 'NoBin'
+                    # Write all information to a file
+                    new_info = '\t'.join([quer, refer, quer_node,
+                                          reference_node, ani, orths,
+                                          total, quer_bin, ref_bin]) + '\n'
+                    o.write(new_info)
+                else:
+                    pass
+                constant_line = af.readline()
     return writelist
 
 
-
-
-def write_new_ani(binlist, anifile, output):
-    writelist = append_bins_to_ani(binlist, anifile)
-    with open(output, 'w') as o:
-        for line in writelist:
-            o.write(line + '\n')
-    print('Done')
-
-
-mylist = ['Bin-Files/W1-bins.txt', 'Bin-Files/W2-bins.txt',
-          'Bin-Files/W3-bins.txt', 'Bin-Files/W4-bins.txt',
-          'Bin-Files/R1-bins.txt', 'Bin-Files/R2-bins.txt',
-          'Bin-Files/R3-bins.txt', 'Bin-Files/R4-bins.txt']
-# file = 'W1/W1.R1.ANI.5000f.txt'
-# qb = 'Bin-Files/W1-bins.txt'
-# rb = 'Bin-Files/R1-bins.txt'
-
-
-write_new_ani(mylist, 'Skeleton/All.master.5000.txt',
-              'Skeleton/All.5000.txt')
+if __name__ == "__main__":
+    append_bins_to_ani(binlist, argument.ANI, argument.Output)
